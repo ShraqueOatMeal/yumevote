4d8181e5efcd957bfce289b6c80f0245ce8c1a71
ShraqueOatMeal <110514448+ShraqueOatMeal@users.noreply.github.com>
2024-08-18 10:17:22 +0800

    Update Vote.jsx

--- voting-system/src/Vote.jsx
+++ voting-system/src/Vote.jsx
@@ -14,7 +14,7 @@ const Vote = () => {
   useEffect(() => {
     const fetchVoteData = async () => {
       try {
-        const response = await fetch(`${process.env.REACT_APP_API_URL}/results`);
+        const response = await fetch(`https://voting_system_yumevote.htetaung.com/api/results`);
         const data = await response.json();
         const formattedData = data.map(item => ({
           name: item.CandidateName,
